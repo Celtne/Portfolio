3608efd64ab8b9d21606102ad007226096ed0588
Celtne <157726123+Celtne@users.noreply.github.com>
2024-10-29 16:12:25 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,41 @@
-let xLoc = [], yLoc = [], numSegments = 100;
+let vid;
+let vScale = 8;
+let sound;
 
+function preload() {
+  sound = loadSound('(Audio) torn_wings_cant_fly.mp3');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER)
+  createCanvas(640, 480);
+  background(200);
+  pixelDensity(1);
   
-  //initialize the arrays
-  for(let i = 0; i<numSegments; i++){
-    xLoc[i] = width/2;
-    yLoc[i] = height/2; 
-  }
+  vid = createCapture(VIDEO, {flipped: true});
+  vid.size(width / vScale, height / vScale);
+    //vid.hide();
   
-  
-  fill(0);
-  stroke(255);
-  
+     //vid.pixelDensity(1);
+  sound.play();
+  sound.loop();
 }
 
 function draw() {
-  background(0, 20);
   
+  vid.loadPixels();
   
-  xLoc[numSegments] = mouseX;
-  yLoc[numSegments] = mouseY;
-  
-  // xLoc[numSegments] =  xLoc[numSegments] + random(-50,50);
-  // yLoc[numSegments] = yLoc[numSegments] + random(-50,50);
-  
-  
-//   xLoc[numSegments-1] = random(width);
-//   yLoc[numSegments-1] = random(height);
-  
-  for(let i = 0; i<numSegments; i++){
-    
-    xLoc[i] = xLoc[i+1];
-    yLoc[i] = yLoc[i+1];
-    
-    let diameter = sin(map(i,0,numSegments, 0,PI))*150;
-    
-    let r = sin(map(i,0,numSegments, 0,PI))*255;
-    let b = 200 - (cos(map(i,0,numSegments, 10,PI*4))*200);
-    
-//     r = r + random(-30,30);
-    
-//     b = b + random(-30,30);
-    
-    stroke(r, random(200),b );
-    
-    strokeWeight(random(0,1.5));
-    
-    fill(0,0)
-    rect(xLoc[i],yLoc[i],diameter,diameter);
-    
-    
+  for (let x = 0; x < vid.width; x++) {
+    for (let y = 0; y < vid.height; y++) {
+      index = (x + y * vid.width) * 4;
+      
+      let r = vid.pixels[index];     
+      
+
+      fill(220, random(r), 150); 
+      
+      ellipse(x * vScale, y * vScale, vScale, vScale);
+    }
   }
- 
   
-  
+  vid.updatePixels();
 }
